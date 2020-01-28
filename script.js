6bf5ebf8df9feff316232f114965547148fa5176
haramari <haramari.rps@gmail.com>
2020-01-28 10:55:17 +0900

    Initial commit with countdown timer

---
+++ script.js
@@ -0,0 +1,27 @@
+//Set the date you are counting down to...
+var countDownDate = new Date("Jan 31, 2020 21:00:00").getTime();
+
+//Update the count down every 1 second
+var x = setInterval(function(){
+
+    //Get today's date and time
+    var now = new Date().getTime();
+
+    //Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    //Display the result in the element with id="countdown"
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    //if the count down is finished, write some text
+    if(distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "Fleshly baked!!";
+    }
+}, 1000);
